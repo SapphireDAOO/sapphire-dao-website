9b6ed1c7d614d47171597c69fbdaa0a08781b16c
Ademola <aademolaoladipo@gmail.com>
2025-03-13 21:39:40 +0100

    add new invoices directory

---
+++ src/app/admin/invoices/page.tsx
@@ -0,0 +1,12 @@
+import InvoicePage from "@/components/action-components/admin/invoice-list";
+import Container from "@/components/Container";
+
+export default function Invoices() {
+  return (
+    <main>
+      <Container>
+        <InvoicePage />
+      </Container>
+    </main>
+  );
+}
