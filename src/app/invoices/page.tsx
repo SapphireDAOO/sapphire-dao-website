d984dc740541d0f0c9007d0f1112dc38ee24a174
Ademola <aademolaoladipo@gmail.com>
2025-06-18 22:51:43 +0100

    remove unused import

--- src/app/invoices/page.tsx
+++ src/app/invoices/page.tsx
@@ -1,6 +1,5 @@
 import AdminInvoices from "@/components/action-components/admin/admin-invoices";
 import Container from "@/components/Container";
-import ProtectedPage from "@/components/ProtectedPage";
 
 export default function Invoices() {
   return (
