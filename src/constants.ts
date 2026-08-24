795128d3c4ff6f33516a243142e53dc6527eff68
Ademola <aademolaoladipo@gmail.com>
2026-08-24 18:58:57 +0100

    fix: restore the studio subgraph URL for Base Sepolia

--- src/constants.ts
+++ src/constants.ts
@@ -150,7 +150,7 @@ export const NOTES_SIGNER_ADDRESS =
 
 export const THE_GRAPH_API_URL: Record<number, string> = {
   [BASE_SEPOLIA]:
-    "https://gateway.thegraph.com/api/deployments/id/Qme7ZmesfZii8iSBho8YRwHCifZEdHjpEQiNeVG1UUiFrU",
+    "https://api.studio.thegraph.com/query/100227/payment-processor-indexer/version/latest",
   [LOCALHOST]: "http://localhost:8000/subgraphs/name/payment-processor-indexer",
 };
 
